126e79ac7766047939e1b03a492006c73149fc24
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2022-03-22 00:43:21 -0700

    Update tremolo.m

--- tremolo.m
+++ tremolo.m
@@ -1,4 +1,24 @@
+function tremFunc = tremoloGen(depth, tempo)
+% This is a file that creates tremolo effect on the sound file. 
 
+% Creator: Zhizhen (Averi) Yu.
 
-% This is a tremolo file. 
-%Creator: Zhizhen (Averi) Yu.
+%A tremolo is a type of low frequency oscillator (LFO) 
+% that rhythmatically modifies the volume of the guitar. 
+%
+% Reference: 
+% https://christianfloisand.wordpress.com/2012/04/18/coding-some-tremolo/
+
+sampleRate = 10400;
+w = 2 * pi / sampleRate;
+
+Modification = @(t) (1 - depth) + depth * (sin(w * t * tempo));
+%depth — the amount of modulation the LFO will apply on to 
+% the original signal — specified in percent.
+%A cap of 10 Hz works well for tremolo
+
+% outputWave = @(x) outputWave(x) * Modification;
+
+tremFunc = Modification;
+
+plot(tremFunc);
