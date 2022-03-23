2b82b7d1f7120e0ceca1fc01ae41b0fd1f8e8e11
Jimmy-Chu-127 <91294557+Jimmy-Chu-127@users.noreply.github.com>
2022-03-22 17:18:06 -0700

    Add files via upload

--- BandRejectFilter.m
+++ BandRejectFilter.m
@@ -1,22 +1,22 @@
-% BandPassFilter(input_wav, Len, Fs, Low_cut, High_cut) by Haozhang Chu
+% BandRejectFilter(input_wav, Len, Fs, Low_cut, High_cut) by Haozhang Chu
 
 % input_wav = an n x 1 matrix containing the waveform
 % Len = the length of input_wav
 % Fs = sampling frequency (our default is 10400)
 % Low_cut = threshold frequency for which the frequency below it will
-% be filtered out.
+% be kept.
 % High_cut = threshold frequency for which the frequency above it wil
-% l be filtered out.
+% l be kept.
 
 % Function Description:
-% This BandPassFilter takes a input_wav with its length. The sampling
-% frequency, Low_cut frequency, and High_cut frequency. The frequency
-% below the Low_cut frequency in the input_wave will be filtered, whi
-% le the frequency above the High_cut frequency will be filtered out.
-% The frequency between the Low_cut and High_cut frequency will be ke
-% pt by this function.
+% This BandRejectFilter takes a input_wav with its length. The sampli
+% ng frequency, Low_cut frequency, and High_cut frequency. The freque
+% ncy below the Low_cut frequency in the input_wave will be kept, whi
+% le the frequency above the High_cut frequency will be kept. The fre
+% quency between the Low_cut and High_cut frequency will be filtered 
+% out by this function.
 
-function output_wav = BandPassFilter(input_wav, Len, Fs, Low_cut, High_cut)
+function output_wav = BandRejectFilter(input_wav, Len, Fs, Low_cut, High_cut)
     out_trans = fft(input_wav);
     f_trans = (0:Len-1)*(Fs/Len);
 
@@ -26,7 +26,9 @@ function output_wav = BandPassFilter(input_wav, Len, Fs, Low_cut, High_cut)
     output = zeros([Len 1]);
 
     for i = 1:Len
-        if(f_shift(i) > Low_cut && f_shift(i) < High_cut)
+        if(f_shift(i) < Low_cut)
+            output(i) = 1;
+        elseif(f_shift(i) > High_cut)
             output(i) = 1;
         else
             output(i) = 0;
@@ -39,5 +41,5 @@ function output_wav = BandPassFilter(input_wav, Len, Fs, Low_cut, High_cut)
     f_shift2 = (0:Len-1)*(Fs/Len);
 
     output_wav = ifft(out_shift2);
-
+    
 end
--- HighPassFilter.m
+++ HighPassFilter.m
@@ -1,3 +1,39 @@
-function output_wav = HighPassFilter(input_wav, Len, Fs, cut_freq)
+% HighPassFilter(input_wav, Len, Fs, cut_freq) by Haozhang Chu
 
+% input_wav = an n x 1 matrix containing the waveform
+% Len = the length of input_wav
+% Fs = sampling frequency (our default is 10400)
+% cut_freq = the frequency above the cut_freq will be kepted by this
+% HighPassFilter, while the frequency below will be filtered out.
+
+% Function Description:
+% This HighPassFilter takes a input_wav with its length. The sampling
+% frequency and the cut_frequency. The frequency above the cut_freque
+% ncy in the input_wave will be kept, while the frequency below the c
+% ut_frequency will be filtered out by this function.
+
+function output_wav = HighPassFilter(input_wav, Len, Fs, cut_freq)
+    out_trans = fft(input_wav);
+    f_trans = (0:Len-1)*(Fs/Len);
+
+    out_shift = fftshift(out_trans);
+    f_shift = (-Len/2 : Len/2 - 1)*(Fs/Len);
+
+    output = zeros([Len 1]);
+
+    for i = 1:Len
+        if(f_shift(i) > cut_freq || f_shift(i) < -cut_freq)
+            output(i) = 1;
+        else
+            output(i) = 0;
+        end
+    end
+
+    filtered_freq = output.*out_shift;
+
+    out_shift2 = fftshift(filtered_freq);
+    f_shift2 = (0:Len-1)*(Fs/Len);
+
+    output_wav = ifft(out_shift2);
+    
 end
