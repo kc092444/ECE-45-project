c0857e84a4ac47803d166eb4b89ac44921efede9
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2022-03-23 17:41:08 -0700

    slider updates

--- PlotLowPassFilter.m
+++ PlotLowPassFilter.m
@@ -1,11 +1,11 @@
 function freq_response = plotLowPassFilter(Len, Low_cut)
     freq_response = zeros([Len 1]);
-    
+
     Fs = 10400; %Sampling frequency by default
     f_shift = (-Len/2 : Len/2 - 1)*(Fs/Len);
 
     for i = 1:Len
-        if(f_shift(i) > -Low_cut && f_shift(i) < Low_cut)
+        if(f_shift(i) < Low_cut)
             freq_response(i) = 1;
         else
             freq_response(i) = 0;
--- plotHighPassFilter.m
+++ plotHighPassFilter.m
@@ -5,7 +5,7 @@ function output = plotHighPassFilter(Len, High_cut)
     f_shift = (-Len/2 : Len/2 - 1)*(Fs/Len);
 
     for i = 1:Len
-        if(f_shift(i) > High_cut || f_shift(i) < -High_cut)
+        if(f_shift(i) > High_cut)
             output(i) = 1;
         else
             output(i) = 0;
